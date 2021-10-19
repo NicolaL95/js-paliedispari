d1208c3678e9d1ce60719590192d8dc5d74d67de
NicolaL95 <nicola.lilla@gmail.com>
2021-10-19 16:51:09 +0200

    Fine lavorazione extra

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
-const es_sel = prompt("premi 1 per la sezione dei palindromi, 2 per la sezione del pari e dispari")
+const es_sel = prompt("premi 1 per la sezione dei palindromi, 2 per la sezione del pari e dispari");
 
 if (es_sel == 1) {
-    let w_chk = prompt("inserisci la parola da controllare");
 
+    let w_chk = prompt("inserisci la parola da controllare");
+    /* La stringa inserita ribaltata */
     let w_rev = w_chk.split('').reverse().join('');
 
-    console.log(w_chk)
-    console.log(w_rev)
-    if (w_rev == w_chk) {
-        window.alert("La parola è palidroma!")
+    function isPalindroma() {
+        /* .replace utilizzato per poter controllare le frasi palindrome */
+        if (w_rev.replace(/\s+/g, '') == w_chk.replace(/\s+/g, '')) {
+            const result = "La parola è palidroma!";
+            return result;
+        }
+        else {
+            const result = "La parola non è palidroma!";
+            return result;
+        }
     }
-    else {
-        window.alert("La parola non è palidroma!")
-    }
+    const i_pal = isPalindroma();
+    window.alert(i_pal);
 }
 
 if (es_sel == 2) {
     let p_d = prompt("scegli pari o dispari")
+    /* Ciclo while per reinserire il valore in caso di errore  */
     while (p_d.toLowerCase() != "pari" && p_d.toLowerCase() != "dispari") {
-        p_d = prompt("scegli pari o dispari")
+        p_d = prompt("scegli pari o dispari");
     }
-    const n_otf = prompt("scegli un numero da 1 a 5")
-    while (parseInt(n_otf) < 1 && parseInt(n_otf) > 5) {
-        p_d = prompt("scegli pari o dispari")
+    const n_otf = prompt("scegli un numero da 1 a 5");
+    while (parseInt(n_otf) < 1 | parseInt(n_otf) > 5) {
+        n_otf = prompt("scegli un numero da 1 a 5");
     }
     function Random1to5() {
         let risultato = Math.floor(Math.random() * 5) + 1;
@@ -32,22 +39,18 @@ if (es_sel == 2) {
     const somma = parseInt(n_otf) + parseInt(cpu_otf);
 
     if (somma % 2 == 0 && p_d.toLowerCase() == "pari") {
-        window.alert("complimenti, hai vinto")
+        window.alert("complimenti, hai vinto");
     }
     else if (somma % 2 == 0 && p_d.toLowerCase() == "dispari") {
-        window.alert("mi dispiace, hai perso")
+        window.alert("mi dispiace, hai perso");
     }
 
     else if (somma % 2 == 1 && p_d.toLowerCase() == "dispari") {
-        window.alert("complimenti, hai vinto")
+        window.alert("complimenti, hai vinto");
     }
 
     else if (somma % 2 == 1 && p_d.toLowerCase() == "pari") {
-        window.alert("mi dispiace, hai perso")
+        window.alert("mi dispiace, hai perso");
     }
 
-    console.log(n_otf)
-    console.log(cpu_otf)
-    console.log(somma)
-    console.log(p_d)
 }
